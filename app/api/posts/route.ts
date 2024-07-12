ae3b767fffefdcbc44685c53678e3f91e73a8750
Cliff-Ezra <cliffezra212@gmail.com>
2024-07-12 20:29:25 +0300

    `refactor:` project cleanup

--- app/api/posts/route.ts
+++
@@ -1,3 +0,0 @@
-export async function GET() {
-  return Response.json({ message: "We got the Posts!" });
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,7 @@
-import createPost from "@/server/actions/create-post";
-import getPosts from "@/server/actions/get-posts";
-import { posts } from "@/server/schema";
-import Image from "next/image";
-import PostButton from "./components/post-button";
-
-// export const dynamic = "force-dynamic"
-
 export default async function Home() {
-  const { error, success } = await getPosts();
-  if (error) {
-    throw new Error(error);
-  }
-  if (success)
-    return (
-      <main>
-        <h1> Welcome to Next.js</h1>
-        {success.map((posts) => (
-          <div key={posts.id}>
-            <h2>{posts.title}</h2>
-          </div>
-        ))}
-        <form action={createPost}>
-          <input
-            className="bg-black"
-            type="text"
-            name="title"
-            placeholder="Title"
-          />
-          <PostButton />
-        </form>
-        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-      </main>
-    );
+  return (
+    <main>
+      <h1>HomePage</h1>
+    </main>
+  );
 }
