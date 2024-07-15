95205f43dc87f448fb628eda434cf258059a54cb
Cliff-Ezra <cliffezra212@gmail.com>
2024-07-15 23:36:41 +0300

    `feat:` initialize shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default async function Home() {
   return (
     <main>
       <h1>HomePage</h1>
+      <Button className="ml-2" variant={"default"}>Click me</Button>
     </main>
   );
 }
