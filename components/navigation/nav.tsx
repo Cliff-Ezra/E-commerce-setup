a9749bdb59529a0585a9572ed1ffdf603de4c464
Cliff-Ezra <cliffezra212@gmail.com>
2024-07-16 17:14:26 +0300

    `feat:` create nav component and use authenticated user data

---
+++ components/navigation/nav.tsx
@@ -0,0 +1,20 @@
+import { auth } from "@/server/auth";
+import { UserButton } from "./user-button";
+
+export default async function Nav() {
+  const session = await auth();
+//   console.log(session);
+
+  return (
+    <header className="bg-slate-500 py-4">
+      <nav>
+        <ul className="flex justify-between">
+          <li>Logo</li>
+          <li>
+            <UserButton expires={""} user={session?.user} />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
