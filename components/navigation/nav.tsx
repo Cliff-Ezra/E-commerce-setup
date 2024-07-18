b9af6e5c4382093e030a4a5df97174d5de05e12a
Cliff-Ezra <cliffezra212@gmail.com>
2024-07-18 04:29:41 +0300

    `feat:` ternary operator to show login button

--- components/navigation/nav.tsx
+++ components/navigation/nav.tsx
@@ -1,18 +1,32 @@
 import { auth } from "@/server/auth";
 import { UserButton } from "./user-button";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { LogIn } from "lucide-react";
 
 export default async function Nav() {
   const session = await auth();
-//   console.log(session);
+  //   console.log(session);
 
   return (
-    <header className="bg-slate-500 py-4">
+    <header className="py-8">
       <nav>
-        <ul className="flex justify-between">
+        <ul className="flex justify-between px-6">
           <li>Logo</li>
-          <li>
-            <UserButton expires={""} user={session?.user} />
-          </li>
+          {!session ? (
+            <li>
+              <Button asChild>
+                <Link href="/auth/login" className="flex gap-2">
+                  <LogIn size={16} />
+                  <span>Login</span>
+                </Link>
+              </Button>
+            </li>
+          ) : (
+            <li>
+              <UserButton expires={session?.expires} user={session?.user} />
+            </li>
+          )}
         </ul>
       </nav>
     </header>
