a2690db923b1e1707362e924bb69f71dc0173358
Cliff-Ezra <cliffezra212@gmail.com>
2024-07-23 14:00:49 +0300

    `feat:` set up token action to generate verification token and utilize the code on the email-register action

--- server/actions/email-register.ts
+++ server/actions/email-register.ts
@@ -6,25 +6,43 @@ import { eq } from "drizzle-orm";
 import { createSafeActionClient } from "next-safe-action";
 import { db } from "..";
 import { users } from "../schema";
+import { generateEmailVerificationToken } from "./tokens";
 
 const action = createSafeActionClient();
 
 export const emailRegister = action(
   RegisterSchema,
   async ({ first_name, last_name, email, phone, password }) => {
+    // Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(hashedPassword);
+
+    // Check if the user is already in the db
     const existingUser = await db.query.users.findFirst({
       where: eq(users.email, email),
     });
 
     // Check if email is already in the db then say it's in use, else if it is not register the user and send the verification email
     if (existingUser) {
-      // if(!existingUser.emailVerified) {
-      //     const verificationToken =
-      // }
+      if (!existingUser.emailVerified) {
+        const verificationToken = await generateEmailVerificationToken(email);
+        //   await sendVerificationEmail()
+
+        return { success: "Email confirmation resent" };
+      }
       return { error: "Email already in use" };
     }
-    return { success: `Yay!! ${email}` };
+    // Logic for when the user is not registered
+    await db.insert(users).values({
+      email,
+      name: `${first_name} ${last_name}`,
+      phone,
+      password: hashedPassword,
+    });
+
+    const verificationToken = await generateEmailVerificationToken(email);
+
+    // await sendVerificationEmail();
+
+    return { success: "Confirmation Email Sent!" };
   }
 );
---
+++ server/actions/tokens.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { eq } from "drizzle-orm";
+import { db } from "..";
+import { verificationTokens } from "../schema";
+
+export const getVerificationTokenByEmail = async (email: string) => {
+  try {
+    const verificationToken = await db.query.verificationTokens.findFirst({
+      where: eq(verificationTokens.token, email),
+    });
+    return verificationToken;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const generateEmailVerificationToken = async (email: string) => {
+  const token = crypto.randomUUID();
+  const expires = new Date(new Date().getTime() + 3600 * 100);
+
+  const existingToken = await getVerificationTokenByEmail(email);
+
+  if (existingToken) {
+    await db
+      .delete(verificationTokens)
+      .where(eq(verificationTokens.id, existingToken.id));
+  }
+
+  const verificationToken = await db.insert(verificationTokens).values(({
+      email,
+      token,
+      expires,
+    })
+  );
+};
