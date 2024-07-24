b75f55893133ac9879af126e67b337b2e919f455
Cliff-Ezra <cliffezra212@gmail.com>
2024-07-24 09:59:51 +0300

    `feat:` add error and success messages on register form

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -21,6 +21,8 @@ import { useAction } from "next-safe-action/hooks";
 import { cn } from "@/lib/utils";
 import { useState } from "react";
 import { emailRegister } from "@/server/actions/email-register";
+import { FormError } from "./form-error";
+import { FormSuccess } from "./form-success";
 
 export const RegisterForm = () => {
   const form = useForm<z.infer<typeof RegisterSchema>>({
@@ -35,12 +37,12 @@ export const RegisterForm = () => {
   });
 
   const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
   const { execute, status } = useAction(emailRegister, {
     onSuccess(data) {
-      if (data.success) {
-        console.log(data.success);
-      }
+      if (data.error) setError(data.error);
+      if (data.success) setSuccess(data.success);
     },
   });
 
@@ -155,6 +157,8 @@ export const RegisterForm = () => {
                   </FormItem>
                 )}
               />
+              <FormSuccess message={success} />
+              <FormError message={error} />
               <Button size={"sm"} variant={"link"} asChild>
                 <Link href={"/auth/reset"}>Forgot your password</Link>
               </Button>
