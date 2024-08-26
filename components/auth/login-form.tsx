82d309ba32aee4cba6a1b1450e86d4568ef2ecc6
Cliff-Ezra <cliffezra212@gmail.com>
2024-08-26 20:16:26 +0300

    `feat:` two factor functionality added after settings modification

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -11,6 +11,11 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot
+} from "@/components/ui/input-otp";
 import { cn } from "@/lib/utils";
 import { emailSignIn } from "@/server/actions/email-signin";
 import { LoginSchema } from "@/types/login-schema";
@@ -35,11 +40,14 @@ export const LoginForm = () => {
 
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
+  const [showTwoFactor, setShowTwoFactor] = useState(false);
 
   const { execute, status } = useAction(emailSignIn, {
     onSuccess(data) {
       if (data?.error) setError(data.error);
       if (data?.success) setSuccess(data.success);
+
+      if (data.twoFactor) setShowTwoFactor(true);
     },
   });
 
@@ -58,46 +66,81 @@ export const LoginForm = () => {
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)}>
             <div>
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        placeholder="user@gmail.com"
-                        type="email"
-                        disabled={status === "executing"}
-                        autoComplete="email"
-                      />
-                    </FormControl>
-                    <FormDescription />
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        placeholder="******"
-                        type="password"
-                        disabled={status === "executing"}
-                        autoComplete="current-password"
-                      />
-                    </FormControl>
-                    <FormDescription />
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+              {showTwoFactor && (
+                <FormField
+                  control={form.control}
+                  name="code"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>
+                        We&apos;ve sent you a two factor code to your email
+                      </FormLabel>
+                      <FormControl>
+                        <InputOTP
+                          {...field}
+                          maxLength={6}
+                          disabled={status === "executing"}
+                        >
+                          <InputOTPGroup>
+                            <InputOTPSlot index={0} />
+                            <InputOTPSlot index={1} />
+                            <InputOTPSlot index={2} />
+                            <InputOTPSlot index={3} />
+                            <InputOTPSlot index={4} />
+                            <InputOTPSlot index={5} />
+                          </InputOTPGroup>
+                        </InputOTP>
+                      </FormControl>
+                      <FormDescription />
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              )}
+              {!showTwoFactor && (
+                <>
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            placeholder="user@gmail.com"
+                            type="email"
+                            disabled={status === "executing"}
+                            autoComplete="email"
+                          />
+                        </FormControl>
+                        <FormDescription />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            placeholder="******"
+                            type="password"
+                            disabled={status === "executing"}
+                            autoComplete="current-password"
+                          />
+                        </FormControl>
+                        <FormDescription />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </>
+              )}
               <FormSuccess message={success} />
               <FormError message={error} />
               <Button size={"sm"} className="px-0" variant={"link"} asChild>
@@ -112,7 +155,7 @@ export const LoginForm = () => {
                 status === "executing" ? "animate-pulse" : ""
               )}
             >
-              {"Login"}
+              {showTwoFactor ? "Verify Code" : "Sign In"}
             </Button>
           </form>
         </Form>
--- server/actions/email-signin.ts
+++ server/actions/email-signin.ts
@@ -4,9 +4,13 @@ import { LoginSchema } from "@/types/login-schema";
 import { eq } from "drizzle-orm";
 import { createSafeActionClient } from "next-safe-action";
 import { db } from "..";
-import { users } from "../schema";
-import { sendVerificationEmail } from "./email";
-import { generateEmailVerificationToken } from "./tokens";
+import { twoFactorTokens, users } from "../schema";
+import { sendTwoFactorTokenEmail, sendVerificationEmail } from "./email";
+import {
+  generateEmailVerificationToken,
+  generateTwoFactorToken,
+  getTwoFactorTokenByEmail,
+} from "./tokens";
 import { signIn } from "../auth";
 import { AuthError } from "next-auth";
 
@@ -34,7 +38,34 @@ export const emailSignIn = action(
           verificationToken[0].email,
           verificationToken[0].token
         );
-        return { success: "Confirmation Email Sent! Confirm your email to Sign in" };
+        return {
+          success: "Confirmation Email Sent! Confirm your email to Sign in",
+        };
+      }
+
+      if (existingUser.twoFactorEnabled && existingUser.email) {
+        if (code) {
+          const twoFactorToken = await getTwoFactorTokenByEmail(
+            existingUser.email
+          );
+          if (!twoFactorToken) return { error: "Invalid Token" };
+          if (twoFactorToken.token !== code) return { error: "Invalid Code" };
+
+          const hasExpired = new Date(twoFactorToken.expires) < new Date();
+          if (hasExpired) return { error: "Token has expired" };
+
+          await db
+            .delete(twoFactorTokens)
+            .where(eq(twoFactorTokens.id, twoFactorToken.id));
+        } else {
+          // Generate a new token
+          const token = await generateTwoFactorToken(existingUser.email);
+
+          if (!token) return { error: "Token not generated" };
+
+          await sendTwoFactorTokenEmail(token[0].email, token[0].token);
+          return { twoFactor: "Two Factor Token Sent" };
+        }
       }
 
       await signIn("credentials", { email, password, redirectTo: "/" });
